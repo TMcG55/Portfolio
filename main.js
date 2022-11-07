4090ba5d086fa228c8bbfd620f81ac4c0a67cddb
TMcG55 <101125108+TMcG55@users.noreply.github.com>
2022-11-07 16:57:33 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radioA'+ counter).checked = true;
+    counter++;
+    if(counter>3){
+      counter = 1;
+    }
+},3000)
